9b0a9cf7740ecbd4e3e62586bdb45c6ae698a8d4
simon <ertel.simon+github@gmail.com>
2021-12-14 21:25:47 +0100

    fix mobile display

--- src/components/completed.js
+++ src/components/completed.js
@@ -3,6 +3,7 @@ import { useThematic } from "../store/thematic";
 import { Text } from "../store/lang";
 import { Link } from "react-router-dom";
 import { Hint } from "../utils/index";
+import Footer from "./footer";
 
 const Completed = ({ total }) => {
   const {
@@ -25,51 +26,60 @@ const Completed = ({ total }) => {
   }, [getAllAnswers, thematic]);
 
   return (
-    <div className="congratulation">
-      <h2>
-        <Text tid="Congratulations" />
-      </h2>
-      <p>
-        <Text tid="YouCompletedThe" />
-        <strong>{thematic}</strong>
-        <Text tid="set" />.
-      </p>
-      <p>
-        <Link to="/">
-          <Text tid="GoBackToHome" />
-        </Link>{" "}
-        .&nbsp;
-        <span onClick={refresh}>
-          <Text tid="Redo" />
-        </span>
-      </p>
-      <div>
-        <table className="recap">
-          <tbody>
-            {(() => {
-              let container = [];
-              let i = 0;
-              for (const property in answers) {
-                i++;
-                container.push(
-                  <tr key={i}>
-                    <td><Hint wordhint={property} /></td>
-                    <td>{answers[property].nom}</td>
-                  </tr>
-                );
-              }
-              return container;
-            })()}
-          </tbody>
-          <thead>
-            <tr>
-              <th><Text tid="hint" /></th>
-              <th><Text tid="solutions" /></th>
-            </tr>
-          </thead>
-        </table>
+    <>
+      <div className="congratulation">
+        <h2>
+          <Text tid="Congratulations" />
+        </h2>
+        <p>
+          <Text tid="YouCompletedThe" />
+          <strong>{thematic}</strong>
+          <Text tid="set" />.
+        </p>
+        <p>
+          <Link to="/">
+            <Text tid="GoBackToHome" />
+          </Link>{" "}
+          .&nbsp;
+          <span onClick={refresh}>
+            <Text tid="Redo" />
+          </span>
+        </p>
+        <div>
+          <table className="recap">
+            <tbody>
+              {(() => {
+                let container = [];
+                let i = 0;
+                for (const property in answers) {
+                  i++;
+                  container.push(
+                    <tr key={i}>
+                      <td>
+                        <Hint wordhint={property} />
+                      </td>
+                      <td>{answers[property].nom}</td>
+                    </tr>
+                  );
+                }
+                return container;
+              })()}
+            </tbody>
+            <thead>
+              <tr>
+                <th>
+                  <Text tid="hint" />
+                </th>
+                <th>
+                  <Text tid="solutions" />
+                </th>
+              </tr>
+            </thead>
+          </table>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
